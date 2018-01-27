5602bba210eb8c92037f3e2a6f4c5a84a1b62537
pichoup <teyuan.d.chen@gmail.com>
2018-01-26 22:48:37 -0800

    stuff

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 //player's turn state
-//public enum TurnState
+public enum TurnState {
+    StartOfTurn,
+    MiddleTurn,
+    EndTurn
+}
 
 public class Player : MonoBehaviour {
     public List<Card> playerCards;
+    public List<TrapCard> trapCards;
+    public List<Player> otherPlayers;
     public WinCondition condition;
+    public int amountOfCardsPerTrapCard;
     public bool isTurn;
     public TurnState state;
 
+    public void TradeCardsForTrapCard(CardType ct, int amount)
+    {
+        if (playerCards.Where(x => x.cardType == ct).Count() >= amount)
+        {
+            AddNewTrapCardToInventory();
+        }
+    }
 
+    private void AddNewTrapCardToInventory()
+    {
+        trapCards.Add(new TrapCard());
+    }
 
 }
---
+++ Assets/TrapCard.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapCard : MonoBehaviour {
+
+    public TrapCard() {
+       //do fancy stuff here to pick from a list of trap cards
+    }
+}
