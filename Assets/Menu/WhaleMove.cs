45039d4e638fe78a79f3de5be9cf2d08fab6e9d7
Steven <Steven_Bucholtz@hotmail.com>
2018-01-27 22:33:41 -0800

    Finished Menu
    Isn't it beautiful?

---
+++ Assets/Menu/WhaleMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+public class WhaleMove : MonoBehaviour
+{
+    public Transform farEnd;
+    private Vector3 x;
+    private Vector3 y;
+    private Vector3 turn;
+    private float pass = 30f;
+    bool towards = false;
+
+
+    void Start()
+    {
+        Debug.Log(x = transform.position);
+
+
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(x, y, Mathf.SmoothStep(0f, 1f, Mathf.PingPong(Time.time / pass, 1f)
+          ));
+
+
+
+
+
+
+    }
+}
