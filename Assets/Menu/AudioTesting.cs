97d4ab7dd8c373aa724f74d0e8ae38bd897975b3
pichoup <teyuan.d.chen@gmail.com>
2018-01-28 15:32:49 -0800

    AUDIO OVERHAUL

---
+++ Assets/Menu/AudioTesting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTesting : MonoBehaviour {
+
+    public List<AudioClip> clips;
+    public AudioSource source;
+
+
+    public void OnClick()
+    {
+        int clip = Random.Range(0, clips.Count);
+        source.PlayOneShot(clips[clip]);
+    }
+}
